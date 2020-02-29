360ed55a17a469b1adfaa25c3ced84bd6d01a3af
apisitprompha <apisitprompha@gmail.com>
2020-02-29 20:52:42 +0700

    Add project files.

---
+++ shop_shop/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace shop_shop.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string bookId { get; set; }
+
+        public string bookName { get; set; }
+
+        public decimal price { get; set; }
+
+        public string category { get; set; }
+
+        public string author { get; set; }
+    }
+}
---
+++ shop_shop/Services/BookService.cs
@@ -0,0 +1,41 @@
+﻿using shop_shop.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace shop_shop.Services
+{
+    public class BookService
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookService(IBookstoreDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _books = database.GetCollection<Book>(settings.BooksCollectionName);
+        }
+
+        public List<Book> Get() =>
+            _books.Find(book => true).ToList();
+
+        public Book Get(string id) =>
+            _books.Find<Book>(book => book.bookId == id).FirstOrDefault();
+
+        public Book Create(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void Update(string id, Book bookIn) =>
+            _books.ReplaceOne(book => book.bookId == id, bookIn);
+
+        public void Remove(Book bookIn) =>
+            _books.DeleteOne(book => book.bookId == bookIn.bookId);
+
+        public void Remove(string id) =>
+            _books.DeleteOne(book => book.bookId == id);
+    }
+}
